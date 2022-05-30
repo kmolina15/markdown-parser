f3cc086ee0d3b4ddf6106594183699422f9e94a1
kmolina15 <103554567+kmolina15@users.noreply.github.com>
2022-05-30 14:15:46 -0700

    report4 tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -35,6 +35,46 @@ public class MarkdownParseTest {
     }
 
     @Test
+    public void testBackticks() throws IOException {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("`google.com");
+        expected.add("google.com");
+        expected.add("ucsd.edu");
+
+        Path fileName = Path.of("week8-test1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        assertEquals(expected, links);
+    }
+
+    @Test
+    public void testNested() throws IOException {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("a.com");
+        expected.add("a.com(())");
+        expected.add("example.com");
+
+        Path fileName = Path.of("week8-test2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        assertEquals(expected, links);
+    }
+
+    @Test
+    public void testLineBreaks() throws IOException {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+
+        Path fileName = Path.of("week8-test3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        assertEquals(expected, links);
+    }
+
+    @Test
     public void testMyFile1() throws IOException {
         ArrayList<String> expected = new ArrayList<>();
         expected.add("some-link.html");
